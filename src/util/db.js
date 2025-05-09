6bcfafcfbaa89f0c8c48db11e2b71806caa675b5
s0urce-c0de <email.kanavg@gmail.com>
2025-05-09 15:40:12 -0700

    d

--- src/util/db.js
+++ src/util/db.js
@@ -6,6 +6,7 @@ export const pool = new Pool({
   host: process.env.DB_HOST,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
 });
 
 function hashIp(ip) {
